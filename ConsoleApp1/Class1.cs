b8c67724ac7b293a59647df44a3fea8d84ceea77
TRAINEE - BIHAG <zabihag@n-pax.com>
2025-08-22 08:18:59 +0800

    added helo git prog

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+       
+        
+
+        private static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("HEllo git");
+           
+
+            Console.ReadKey();
+        }
+
+        
+
+        
+       
+
+    }
+}
